85ae6c8ae2188e962569e10c744551f0b745e19c
Ramirez <ramirez.alberto@zoho.com>
2022-09-18 21:44:17 -0700

    Agregar archivos de proyecto.

---
+++ AccountOwnerServer/Extensions/ServiceExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace AccountOwnerServer.Extensions
+{
+    public static class ServiceExtensions
+    {
+        /*
+         An extension method is inherently the static method.
+        They play a great role in .NET Core configuration because they increase the readability of our code for sure.
+        What makes it different from the other static methods is that it accepts “this” as the first parameter,
+        and “this” represents the data type of the object which uses that extension method.
+        An extension method must be inside a static class.
+        This kind of method extends the behavior of the types in .NET.*/
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod());
+            });
+        }
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+        }
+    }
+}
---
+++ AccountOwnerServer/Program.cs
@@ -0,0 +1,33 @@
+using AccountOwnerServer.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+if(app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");  // Call above UseAuthorization
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
