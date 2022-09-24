f767568f737dd7c7902c1e8391e832860a0acbae
Ramirez <ramirez.alberto@zoho.com>
2022-09-23 20:05:07 -0700

    Implement search by Name

--- Entities/Models/OwnerParameters.cs
+++ Entities/Models/OwnerParameters.cs
@@ -6,6 +6,7 @@ namespace Entities.Models
         public uint MinYearOfBirth { get; set; }
         public uint MaxYearOfBirth { get; set; } = (uint)DateTime.Now.Year;
         public bool isValidDate() => MaxYearOfBirth > MinYearOfBirth;
+        public string Name { get; set; } = "";
 
     }
 }
--- Repository/OwnerRepository.cs
+++ Repository/OwnerRepository.cs
@@ -16,13 +16,25 @@ namespace Repository
             .OrderBy(ow => ow.Name)
             .ToListAsync();
 
-        public async Task<PagedList<Owner>> GetOwnersAsync(OwnerParameters ownerParameters) =>
-            await PagedList<Owner>.ToPagedListAsync(
-                FindByCondition(o => o.DateOfBirth.Year >= ownerParameters.MinYearOfBirth &&
-                    o.DateOfBirth.Year <= ownerParameters.MaxYearOfBirth)
+        public async Task<PagedList<Owner>> GetOwnersAsync(OwnerParameters ownerParameters)
+        {
+            var owners = FindByCondition(o => o.DateOfBirth.Year >= ownerParameters.MinYearOfBirth &&
+                    o.DateOfBirth.Year <= ownerParameters.MaxYearOfBirth);
+            SearchByName(ref owners, ownerParameters.Name);
+
+            return await PagedList<Owner>.ToPagedListAsync(owners                
                 .OrderBy(ow => ow.Name)
                 ,ownerParameters.PageNumber
                 ,ownerParameters.PageSize);
+        }
+
+        private void SearchByName(ref IQueryable<Owner> owners, string ownerName)
+        {
+            if(!owners.Any() || string.IsNullOrWhiteSpace(ownerName))
+                return;
+
+            owners = owners.Where(o => o.Name.ToLower().Trim().Contains(ownerName.Trim().ToLower()));
+        }
 
         public async Task<Owner?> GetOwnerByIdAsync(Guid id) =>
             await FindByCondition(ow => ow.Id.Equals(id)).FirstOrDefaultAsync();
